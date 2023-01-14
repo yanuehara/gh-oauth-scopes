f1c1cbba8868cdb34b04fed0ec89c88ea646d038
Yan Uehara <yanuehara@hotmail.com>
2023-01-14 12:57:28 -0300

    Print x-oauth-scopes from autoconfig

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
+// Copyright (c) 2023 @yanuehara (https://github.com/yanuehara)
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/cli/go-gh"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-auth-scopes extension!")
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
+
+	resp, err := client.Request("GET", "https://api.github.com/user", nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("running as %s\n", response.Login)
-}
+	defer resp.Body.Close()
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+        fmt.Println(err)
+		return
+    }
+
+	type User struct{
+		Login string `json:login`
+	}
+	var data User
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Acessing github as @%s with oauth scopes %s", data.Login, resp.Header.Get("X-Oauth-Scopes"))
+}
